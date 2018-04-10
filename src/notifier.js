52fa17d082bd85ede54d9fc638a8199367e1e48d
grozamorei <desperatebassplayer@gmail.com>
2018-04-10 16:46:02 +0300

    json parse error fix

--- src/notifier.js
+++ src/notifier.js
@@ -9,37 +9,44 @@ module.exports = (config, onChannelLive, onChannelLeaves) => {
                 'Client-ID': config.tw.client_id
             }
         }, (err, _, body) => {
-            const json = JSON.parse(body)
-            if (json['channels']) {
-                if (json['channels'].length > Object.keys(channels).length) {
-                    // console.log('new channel online! ')
-                    // channels = json['channels']
-                    let newChannels = []
-                    json['channels'].forEach(ch => {
-                        if (ch.id in channels) return
-                        channels[ch.id] = {
-                            link: 'https://twitch.tv/' + ch.username,
-                            game: ch.game,
-                            view_count: ch.view_count
-                        }
-                        newChannels.push(channels[ch.id])
-                    })
-                    newChannels.forEach(onChannelLive)
+            try {
+                const json = JSON.parse(body)
+                if (json['channels']) {
+                    if (json['channels'].length > Object.keys(channels).length) {
+                        // console.log('new channel online! ')
+                        // channels = json['channels']
+                        let newChannels = []
+                        json['channels'].forEach(ch => {
+                            if (ch.id in channels) return
+                            channels[ch.id] = {
+                                link: 'https://twitch.tv/' + ch.username,
+                                game: ch.game,
+                                view_count: ch.view_count
+                            }
+                            newChannels.push(channels[ch.id])
+                        })
+                        newChannels.forEach(onChannelLive)
+                    }
+                    if (json['channels'].length < Object.keys(channels).length) {
+                        const oldChannels = Object.assign({}, channels)
+                        json['channels'].forEach(ch => {
+                            if (ch.id in oldChannels) {
+                                delete oldChannels[ch.id]
+                            }
+                        })
+                        Object.keys(oldChannels).forEach(key => {
+                            onChannelLeaves(oldChannels[key])
+                            delete channels[key]
+                        })
+                    }
                 }
-                if (json['channels'].length < Object.keys(channels).length) {
-                    const oldChannels = Object.assign({}, channels)
-                    json['channels'].forEach(ch => {
-                        if (ch.id in oldChannels) {
-                            delete oldChannels[ch.id]
-                        }
-                    })
-                    Object.keys(oldChannels).forEach(key => {
-                        onChannelLeaves(oldChannels[key])
-                        delete channels[key]
-                    })
-                }
+                setTimeout(work, 10000)
+            } catch (e) {
+                console.log('wrong json: ', body)
+                setTimeout(work, 10000)
+                return
             }
-            setTimeout(work, 10000)
+
         })
     }
     work()
